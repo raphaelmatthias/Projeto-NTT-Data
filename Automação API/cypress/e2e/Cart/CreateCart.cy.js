6790d218ef59126cbc9292bdf99249a942e4e11d
Raphaelmatthias <contatoraphaelmathias@gmail.com>
2025-04-23 11:36:09 -0300

    Melhorias Automações
    Hoje pela manhã foi visto que o sistema apaga os dados conforme passam os dias então os logins seriam perdidos, foram boladas estratégias para não ocorrerem esses erros.

--- Automação API/cypress/e2e/Cart/CreateCart.cy.js
+++ Automação API/cypress/e2e/Cart/CreateCart.cy.js
@@ -3,8 +3,9 @@ import CreateCartMethods from "../../support/Methods/CreateCartMethods"
 
 describe('Cadastro de carrinho via API', () => {
   it('Criar novo carrinho checando se já há um cadastrado anteriormente', () => {
-
-   cy.loginEGuardarToken("contatoraphaelmathias@gmail.com", "raphael.123"); 
+  
+  cy.loginEGuardarToken("contatoraphaelmathias@gmail.com", "raphael.123"); 
+   cy.recuperarIDUsuario("contatoraphaelmathias@gmail.com");
    CreateCartMethods.CreateCart("BeeJh5lz3k6kSIzA", "1", "c29kcL2tLUuhLvs6");
   });
 });
